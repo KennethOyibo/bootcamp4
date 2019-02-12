abb8c2adc2c1073fd8a2f7f7933d0a3904e80075
KennethOyibo <k.oyibo@ufl.edu>
2019-02-11 23:53:57 -0500

    initialize commit

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,19 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://warm-retreat-24285.herokuapp.com/api/listings');
+      return $http.get('http://localhost:8080/api/listings');
     },
-	
-	create: function(listing) {
-	  return $http.post('https://warm-retreat-24285.herokuapp.com/api/listings/', listing);
+  
+  create: function(listing) {
+    return $http.post('http://localhost:8080/api/listings', listing);
     }, 
 
-    delete: function(index) {
-	   /**TODO
+    delete: function(_id) {
+     /**TODO
         return result of HTTP delete method
        */
-    //return $http.delete('https://warm-retreat-24285.herokuapp.com/api/listings/' + id);
-    return $http.delete('https://warm-retreat-24285.herokuapp.com/api/listings/',index);
+    return $http.delete('http://localhost:8080/api/listings/'+_id);
 
     }
   };
