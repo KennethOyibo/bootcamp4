8d3ec1bf5fdb81afd613c1888fedb720eeeb65d9
KennethOyibo <k.oyibo@ufl.edu>
2019-02-11 19:13:41 -0500

    initialize commit

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -18,12 +18,11 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     $scope.deleteListing = function(index) {
       //var indexOf = $scope.listings.indexOf(index);
+      $scope.listings.splice(index,1);
       Listings.delete(index).then(function(response) {
     }, function(error) {
       console.log('Could not delete:', error);
     });
-
-      $scope.listings.splice(indexOf,1);
     };
     
     $scope.showDetails = function(index) {
